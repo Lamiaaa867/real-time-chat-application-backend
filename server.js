9d6628b4beea6f1215eeed68335e82e8fc3d3b15
lamiaa_emad <84296897+Lamiaaa867@users.noreply.github.com>
2025-04-24 06:39:34 +0200

    update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import {app, server} from './socket/socket.js'
 dotenv.config()
 
 conectDB()
-const port= process.env.PORT|| 5000
+const port= process.env.PORT|| 8080
 
 app.use(express.json())
 app.use(cookieParser())
